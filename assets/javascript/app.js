5746aaef8b0d1e5bd70c6368e6a14a2aff68ee49
Milon <miloncl@gmail.com>
2018-09-22 23:53:46 -0700

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 		count: function() {
 				countdownTimer.time--;
 				console.log(countdownTimer.time);
-//				$('.timer').html(countdownTimer.time);
+
 			if (countdownTimer.time >= 0) {
 				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
 			}
@@ -27,66 +27,13 @@ $(document).ready(function() {
 				if (index < questionArray.length) {
 					loadQuestion(index);
 				} else {
-					$(".answers").hide();
+					$(".answer").hide();
 					showScore();
 				}
 			}
 		}
 	};
-/*
-	var trivia = {
-		questions: ['Who was the host for Kitchen Kabaret?',
-								'Who starred in the title role of Condorman?',
-								'In Captain EO, what is the name of the elephant like alien?',
-								'The 1980\'s were big for new personal computer introductions. Which of the following computers was introduced in 1980?',
-								'What country was welcomed into the World Showcase at EPCOT Center in 1984?',
-								'In 1981 a Disney constructed WED-Way People Mover opened at which airport?',
-								'What school is attended in the Disney Channel Series, Girl Meets World?',
-								'Which Walt Disney World location opened on the same day as the Disney-MGM Studios theme park on May 1, 1989?',
-								'Which of the following films is NOT part of the Walt Disney Studios Silly Symphonies?',
-								'Which wartime activity did the Walt Disney Studios partake in to support the American war effort?'],
-		q1: ['A. Fud Wrapper',
-				 'B. Cookie Ann Milk',
-				 'C. Bonnie Appetit',
-				 'D. Cherry Ontop'],
-		q2: ['A. Zac Efron',
-				 'B. Michael Crawford',
-				 'C. Billy Crystal',
-				 'D. Michael Keaton'],
-		q3: ['A. Trunks',
-				 'B. Hooter',
-				 'C. Elle',
-				 'D. Odie'],
-		q4: ['A. Sinclair ZX80',
-				 'B. IBM PC',
-				 'C. Commodore 64',
-				 'D. Atari 2600'],
-		q5: ['A. Norway',
-				 'B. Morocco',
-				 'C. France',
-				 'D. Japan'],
-		q6: ['A. Houston Intercontinental Airport',
-				 'B. Orlando International Airport',
-				 'C. Atlanta International Airport',
-				 'D. Dallas/Ft. Worth International Airport'],
-		q7: ['A. Vintage High School',
-				 'B. Peyton Middle School',
-				 'C. John Quincy Adams Middle School',
-				 'D. Washington High School'],
-		q8: ['A. Typhoon Lagoon',
-				 'B. Pleasure Island',
-				 'C. Both A & B',
-				 'D. None of the above'],
-		q9: ['A. The Night Before Christmas',
-				 'B. Three Little Pigs',
-				 'C. The Old Mill',
-				 'D. The Gallopin\' Gaucho'],
-		q10: ['A. Recycling used film footage',
-				  'B. Designing US Army & US Navy insignia',
-				  'C. Hosted a Studio Victory Garden where employees grew food for their families',
-				  'D. Forced employees to carpool by closing parking lots to non-carpool cars']		 						
-	}
-*/
+
 var correct = 0;
 var wrong = 0;
 var question1 = {
@@ -199,14 +146,8 @@ function loadQuestion(questionSelection) {
   $("#buttonB").text(questionArray[questionSelection].answers[1]).show();
   $("#buttonC").text(questionArray[questionSelection].answers[2]).show();
   $("#buttonD").text(questionArray[questionSelection].answers[3]).show();
-//  getAnswer();  
-//  nextQuestion(index);
-}
 
-//function nextQuestion() {
-//	index = index++;
-//	console.log(index);
-//}
+}
 
 function setup() {
 	index = 0;
@@ -220,7 +161,6 @@ function setup() {
 
 function getAnswer() {
 
-//  nextQuestion();
 	$('.answer').on('click', function() {
 	  console.log('alert', index);
 		index++;
@@ -235,14 +175,16 @@ function getAnswer() {
 }
 
 function answerCorrect() {
-	correct++;
-	alert("Correct!");
+    correct++;
+        $(".answers").hide();
+        $(".question").text("Correct Answer");
 	console.log("correct");
 }
 
 function answerWrong() {
-	wrong++;
-	alert("Incorrect!");
+    wrong++;
+        $(".answers").hide();
+        $(".question").text("Wrong Answer");
 	console.log("wrong");
 }
 
@@ -254,29 +196,6 @@ function showScore() {
 	$('.timer').empty();
 
 }
-//		for (var i=0; i<questionArray.length; i++) {
-//			$('.question').append('<p>'+questionArray[i].question+'</p>');
-//			for (var j=0; j<questionArray[i].possibleAnswers.length; j++) {
-//				$('.answers').append('<span><button id="possibleAnswer">' + questionArray[i].possibleAnswers[j]+ '</button></span>');
-//			}
-//			$('#possibleAnswers').on('click', function() {
-
-
-//		console.log("click");
-//		countdownTimer.start();
-//		for (var i = 0; i < questionArray.length; i++) {
-//			console.log(i);
-
-//			$('.timer').html('<h3>'+countdownTimer.time + ' seconds remaining</h3>');
-//			$('.question').html(questionArray[i].question);
-//			while (countdownTimer != 0) {
-
-//			}
-		
-//	});
-//	$('#startButton').click(countdownTimer.start);
-
-//}
 var answerChosen
 setup();
 $('.answers').on('click', function() {
@@ -291,37 +210,24 @@ $('.answers').on('click', function() {
  	answerChosen = 'D';
  } 
  if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
- 	answerCorrect();
+ 	setTimeout(answerCorrect(), 3000);
  } else if (answerChosen == 'A') {
- 	answerWrong();
+    setTimeout(answerWrong(), 3000);
  }
  if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
- 	answerCorrect();
+    setTimeout(answerCorrect(), 3000);
  } else if (answerChosen == 'B') {
- 	answerWrong();
+    setTimeout(answerWrong(), 3000);
  }
 if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
- 	answerCorrect();
+    setTimeout(answerCorrect(), 3000);
  } else if (answerChosen == 'C') {
- 	answerWrong();
+    setTimeout(answerWrong(), 3000);
  }
 if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
- 	answerCorrect();
+    setTimeout(answerCorrect(), 3000);
  } else if (answerChosen == 'D') {
- 	answerWrong();
- }
-
- $(".question").text('');
- $("#buttonA").text('');
- $("#buttonB").text('');
- $("#buttonC").text('');
- $("#buttonD").text('');
- index++;
- if (index < questionArray.length) {
- 	loadQuestion(index);
- } else {
- 	$(".answerchoice").hide();
- 	showScore();
+    setTimeout(answerWrong(), 3000);
  }
 });
 
